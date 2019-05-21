faa36fcd0ebd48763ffe90102e5c536fca6945c1
lorenzomammana <lorenzom96@hotmail.it>
2019-05-21 11:56:16 +0200

    Fix latex spacing, update report, minor changes

--- Report/compile_book.R
+++ Report/compile_book.R
@@ -1,7 +1,21 @@
+pkgTest <- function(x)
+{
+  if (!require(x, character.only = TRUE))
+  {
+    install.packages(x, dep = TRUE)
+    if (!require(x, character.only = TRUE))
+      stop("Package not found")
+  }
+}
+
+pkgTest("pander")
+pkgTest("psych")
+pkgTest("survival")
+pkgTest("survminer")
+
 fn <- "_main.Rmd"
-if (file.exists(fn)) 
+if (file.exists(fn))
   file.remove(fn)
 
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 bookdown::render_book("index.Rmd")
-
